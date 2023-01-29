2d425992b648ae04f3ca9404cf158e4c200f8691
Hayato Midorikawa <>
2023-01-29 15:03:55 +0900

    implement css-loader and style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,17 @@ module.exports = {
     path: outputPath,
     filename: 'main.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
   mode: 'production',
   devServer: {
     contentBase: outputPath
