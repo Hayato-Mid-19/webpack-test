8f626bcb868c46d0b95f760eaba5e05079131072
Hayato Midorikawa <>
2023-01-29 13:25:36 +0900

    implement some module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import _ from 'lodash';
+// import {Name, Nijou} from './utilities';
+// import * as utilities from './utilities';
+import {Name as Name_obj} from './utilities';
+import Lion from './utilities';
+
+// console.log(Nijou(8));
+// console.log(Name);
+
+// console.log(utilities.Name)
+
+console.log(Name_obj)
+console.log(Lion.say())
 
 const doc = document;
 function conponent () {
@@ -8,4 +20,5 @@ function conponent () {
   element.innerHTML = _.join(array, ' ');
   return element;
 }
+
 doc.body.appendChild(conponent());
